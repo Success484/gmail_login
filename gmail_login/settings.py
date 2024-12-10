d6dfc2c2730c2468c91fb24db77699e0ae8f7553
Success <successsimeon484@gmail.com>
2024-12-10 11:29:36 +0100

    feat: deploying_to_vercel

--- gmail_login/settings.py
+++ gmail_login/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','gmail-login.onrender.com']
+ALLOWED_HOSTS = ['.vercel.app','localhost','gmail-login.onrender.com']
 
 
 # Application definition
